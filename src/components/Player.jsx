7d30b8bb831be21046920f281a8f2e864f8ecec9
David Regaldi <david.regaldi@gmail.com>
2026-01-03 22:11:31 +0100

    Repetition: Managing User Input with State (Two-Way-Binding).
    Old method.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, SetEnteredPlayerName] = useState('');
+  const [submited, setSubmited] = useState(false)
+
+  function handleChange(event) {
+    setSubmited(false)
+    SetEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmited(true)
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submited ? enteredPlayerName : 'Unknown Entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
