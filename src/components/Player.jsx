7d2ca052f99e922e77fbc0fcd411888156ffec16
David Regaldi <david.regaldi@gmail.com>
2026-01-03 23:16:22 +0100

    Personal optimisations.
    onClick reset and spacing gestion on input.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,16 +5,18 @@ export default function Player() {
   const [enteredPlayerName, SetEnteredPlayerName] = useState(null);
 
   function handleClick() {
-    SetEnteredPlayerName(playerName.current.value || null);
+    SetEnteredPlayerName(playerName.current.value.trim());
+    playerName.current.value = playerName.current.value.trim();
   }
 
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? 'Unknown Entity'}</h2>
+      <h2>Welcome {enteredPlayerName || 'Unknown Entity'}</h2>
       <p>
         <input
           ref={playerName}
           type="text"
+          onClick={() => playerName.current.value = ''}
         />
         <button onClick={handleClick}>Set Name</button>
       </p>
